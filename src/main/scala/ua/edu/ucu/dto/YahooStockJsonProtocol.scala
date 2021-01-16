a48fdc8a27b84111dfa3e8d76d27f4ba21efb1a2
Solomiya <solomiya.branets@monese.com>
2021-01-17 01:10:01 +0200

    Stock monitor as publisher (host/stream)

--- src/main/scala/ua/edu/ucu/dto/YahooStockJsonProtocol.scala
+++ src/main/scala/ua/edu/ucu/dto/YahooStockJsonProtocol.scala
@@ -4,15 +4,18 @@ import spray.json.DefaultJsonProtocol
 
 
 case class Root(chart: Chart) {
-  def apply() = chart()
+  def apply() = Tuple2(chart(), chart.getSymbol())
 }
 
 case class Chart(result: List[Result]) {
   def apply() = result(0)()
+  def getSymbol() = result(0).getSymbol()
 }
 
-case class Result(indicators: Indicators) {
+case class Result(indicators: Indicators,
+                  meta: Meta) {
   def apply() = indicators()
+  def getSymbol() = meta()
 }
 
 
@@ -24,13 +27,15 @@ case class Quote(close: Seq[Double]) {
   def apply() = close
 }
 
-case class Meta(symbol: String)
+case class Meta(symbol: String) {
+  def apply() = symbol
+}
 
 object YahooStockJsonProtocol extends DefaultJsonProtocol {
   implicit val quoteFormat = jsonFormat1(Quote)
   implicit val indicatorsFormat = jsonFormat1(Indicators)
   implicit val metaFormat = jsonFormat1(Meta)
-  implicit val resultFormat = jsonFormat1(Result)
+  implicit val resultFormat = jsonFormat2(Result)
   implicit val chartFormat = jsonFormat1(Chart)
   implicit val rootFormat = jsonFormat1(Root)
 
