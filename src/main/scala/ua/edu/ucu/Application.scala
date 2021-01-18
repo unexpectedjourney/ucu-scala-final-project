d5e5ceb5ca49b86ff3102ce4eff4db49cd780164
Anton Babenko <antonwarbeck123@gmail.com>
2021-01-18 22:41:48 +0200

    Added support of multiple companies separated twitter and main application

--- src/main/scala/ua/edu/ucu/Application.scala
+++ src/main/scala/ua/edu/ucu/Application.scala
@@ -4,15 +4,9 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives.{complete, concat, get, path}
-import akka.stream.FlowShape
-import akka.stream.scaladsl.GraphDSL.Implicits.fanOut2flow
-import akka.stream.scaladsl.{Broadcast, BroadcastHub, GraphDSL, Keep, Merge, Source}
+import akka.stream.scaladsl.Source
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import twitter4j.conf.ConfigurationBuilder
-import ua.edu.ucu.dto.Tweet
-import ua.edu.ucu.integrations.TwitterStreamSource
-import ua.edu.ucu.stages.MongoDBSink
 import ua.edu.ucu.utils.Configuration
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
@@ -23,27 +17,6 @@ object Application extends App {
 
   val mapper: ObjectMapper = new ObjectMapper().registerModule(DefaultScalaModule)
   val envs = new Configuration()
-  val config = new ConfigurationBuilder()
-    .setDebugEnabled(true)
-    .setOAuthConsumerKey(envs.twitterAuthConsumerKey)
-    .setOAuthConsumerSecret(envs.twitterAuthConsumerSecret)
-    .setOAuthAccessToken(envs.twitterAuthAccessToken)
-    .setOAuthAccessTokenSecret(envs.twitterAuthAccessTokenSecret)
-    .build()
-
-  private val graphSource = TwitterStreamSource("Tesla", config)
-    .via(GraphDSL.create() { implicit graphBuilder =>
-      val IN = graphBuilder.add(Broadcast[Tweet](1))
-      val TWEET = graphBuilder.add(Broadcast[Tweet](2))
-      val OUT = graphBuilder.add(Merge[Tweet](1))
-
-      IN ~> TWEET                  ~> OUT
-            TWEET ~> MongoDBSink()
-
-      FlowShape(IN.in, OUT.out)
-    })
-    .toMat(BroadcastHub.sink)(Keep.left)
-    .run
 
   val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
     Http().newServerAt("localhost", 8080).connectionSource()
--- src/main/scala/ua/edu/ucu/dto/Tweet.scala
+++ src/main/scala/ua/edu/ucu/dto/Tweet.scala
@@ -1,9 +1,3 @@
 package ua.edu.ucu.dto
 
-import scala.collection.Set
-
-
-case class Tweet(author: Author, timestamp: Long, body: String) {
-  def hashtags: Set[Hashtag] =
-    body.split(" ").collect { case t if t.startsWith("#") => Hashtag(t) }.toSet
-}
+case class Tweet(author: Author, timestamp: Long, body: String, hashtag: Hashtag)
